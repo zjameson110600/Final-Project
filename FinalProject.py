e55d8c27e68f042df7cc904955f4773c611a83c6
Grace Coleman <gracecoleman@Graces-MacBook-Pro-2.local>
2021-04-13 11:33:52 -0400

    have all json data

--- FinalProject.py
+++ FinalProject.py
@@ -1,11 +1,38 @@
 import unittest
 import sqlite3
+import requests
 import json
 import os
 
 #
-# Name: Zita Jameson
-# Who did you work with: Grace Coleman, Giselle Ciulla
+# By: Zita Jameson, Grace Coleman, Giselle Ciulla
 #
 
-def 
+def get_data_from_COVID19(country):
+     url = 'https://coronavirus-19-api.herokuapp.com/countries'
+     parameters = {'country': country}
+     request = requests.get(url, params = parameters)
+     result = request.json()
+     return result
+
+def get_data_from_MEDIAGROUP(country):
+    url = 'https://covid-api.mmediagroup.fr/v1/cases'
+    parameters = {'country': country}
+    request = requests.get(url, params = parameters)
+    result = request.json()
+    return result
+
+def get_data_from_Q(country):
+    url = 'https://public.api.quarantine.country/summary/region'
+    parameters = {'region': country}
+    request = requests.get(url, params = parameters)
+    result = request.json()
+    return result
+
+
+def main():
+    get_data_from_COVID19('USA')
+    get_data_from_MEDIAGROUP('France')
+    get_data_from_Q('usa')
+
+main()
