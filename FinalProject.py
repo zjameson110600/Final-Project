35ade76c232a9d6b4c0694f98413c4cf84e7d453
Grace Coleman <gracecoleman@Graces-MacBook-Pro-2.local>
2021-04-13 12:43:08 -0400

    Working on parsing through json data

--- FinalProject.py
+++ FinalProject.py
@@ -8,31 +8,38 @@ import os
 # By: Zita Jameson, Grace Coleman, Giselle Ciulla
 #
 
-def get_data_from_COVID19(country):
-     url = 'https://coronavirus-19-api.herokuapp.com/countries'
-     parameters = {'country': country}
-     request = requests.get(url, params = parameters)
-     result = request.json()
-     return result
+def get_data_from_COVID19():
+    url = 'https://coronavirus-19-api.herokuapp.com/countries'
+    request = requests.get(url)
+    result = request.json()
+    for c in result:
+        countries = c['country']
+        cases = c['cases']
+        deaths = c['deaths']
+        cases_per_mil = c['casesPerOneMillion']
+        deaths_per_mil = c['deathsPerOneMillion']
 
-def get_data_from_MEDIAGROUP(country):
+def get_data_from_MEDIAGROUP():
     url = 'https://covid-api.mmediagroup.fr/v1/cases'
-    parameters = {'country': country}
-    request = requests.get(url, params = parameters)
+    request = requests.get(url)
     result = request.json()
     return result
 
-def get_data_from_Q(country):
-    url = 'https://public.api.quarantine.country/summary/region'
-    parameters = {'region': country}
-    request = requests.get(url, params = parameters)
+def get_data_from_Q():
+    url = 'https://api.quarantine.country/api/v1/summary/latest'
+    request = requests.get(url)
     result = request.json()
-    return result
+    for c in result['data']['regions']:
+        countries = c['name']
+        cases = c['total_cases']
+        deaths = c['deaths']
+        death_ratio = c['death_ratio']
+    print(result)
 
 
 def main():
-    get_data_from_COVID19('USA')
-    get_data_from_MEDIAGROUP('France')
-    get_data_from_Q('usa')
+    get_data_from_COVID19()
+    get_data_from_MEDIAGROUP()
+    get_data_from_Q()
 
 main()
