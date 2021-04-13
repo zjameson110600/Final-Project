3281b5faff7a10b5d189e05d03bec231ce2df7dc
Zita Jameson <zjameson@umich.edu>
2021-04-13 09:58:37 -0400

    hi

--- FinalProject.py
+++ FinalProject.py
@@ -1,11 +1,34 @@
 import unittest
 import sqlite3
+import requests
 import json
 import os
 
 #
-# Name: Zita Jameson
-# Who did you work with: Grace Coleman, Giselle Ciulla
+# By: Zita Jameson, Grace Coleman, Giselle Ciulla
 #
 
-def 
+def get_data_from_COVID19(from_date, to_date, country):
+    url = 'https://api.covid19api.com/country/{country}/status/confirmed?from={from_date}&to={to_date}}'
+    parameters = {'country': country, 'from': from_date, 'to': to_date}
+    request = requests.get(url, params = parameters)
+    result = request.json()
+    return result
+
+def get_data_from_MEDIAGROUP(country):
+    url = 'https://covid-api.mmediagroup.fr/v1'
+    parameters = {'country': country}
+    request = requests.get(url, params = parameters)
+    result = request.json()
+    return result
+
+def get_data_from_Q(country):
+    url = 'https://public.api.quarantine'
+    parameters = {'country': country}
+    request = requests.get(url, params = parameters)
+    result = request.json()
+    return result
+
+get_data_from_COVID19('2020-07-03T00:00:00Z','2020-07-04T00:00:00Z', 'United States')
+get_data_from_MEDIAGROUP('France')
+get_data_from_Q('usa')
